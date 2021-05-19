4e021acdb03d71bfbeb0217ec3ccf3f6a6a41d83
inayatx <inayatagaria@gmail.com>
2021-05-19 12:13:59 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 //jshint esversion: 6
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
 
 const mockUserData=[
 	{name:'Mark'},
@@ -15,6 +18,27 @@ app.get('/users/:id',function(req,res){
 		message: 'successfully got users. Nice!',
 		user: req.params.id
 	})
+});
+
+app.post('/login',function(req,res){
+ 	const username=req.body.username;
+ 	const password=req.body.password;
+
+ 	const mockUsername="billyTheKid";
+ 	const mockPassword="superSecret";
+
+ 	if (username===mockUsername && password===mockPassword){
+      	res.json({
+      	 	success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+      	})
+ 	} else {
+      	res.json({
+      	 	success: false,
+      	 	message: 'password and username do not match'
+      	})
+ 	}
 });
 
 
